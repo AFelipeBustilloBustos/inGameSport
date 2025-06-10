f0c6827cd97fc9cae3b41067e941bb3d82b6a51b
AFelipeBustilloBustos <wandrex96@gmail.com>
2025-06-10 23:49:12 +0200

    Add files via upload

---
+++ CDOPardillo/assets/js/main.js
@@ -0,0 +1,9 @@
+// Agregar la funcionalidad del menú móvil
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+}); 
